600d0532121880f847dfb7c93b18f8bb0f7f7357
shelbymode <andrew.ananenko@modsen-software.com>
2023-03-17 15:26:56 +0300

    feat(movie): add `movie module to root module`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { PrismaModule } from './prisma/prisma.module'
+import { MovieModule } from './movie/movie.module'
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, MovieModule],
   controllers: [AppController],
   providers: [AppService],
 })
