d50846d6f3f6614cb011b142458a6fd6821cf7ab
shelbymode <andrew.ananenko@modsen-software.com>
2023-03-17 14:22:39 +0300

    feat(swagger): add `swagger docs`

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Modsen-Cinema')
+    .setDescription('The Cinema API description')
+    .setVersion('0.1')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000)
 }
 bootstrap()
