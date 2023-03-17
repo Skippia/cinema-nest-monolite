7dfaadd5d6303f077f9eb528cbfd2cc9b8581479
shelbymode <andrew.ananenko@modsen-software.com>
2023-03-17 15:25:55 +0300

    feat(seat): add `seat module to root module`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { PrismaModule } from './prisma/prisma.module'
+import { SeatModule } from './seat/seat.module'
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, SeatModule],
   controllers: [AppController],
   providers: [AppService],
 })
