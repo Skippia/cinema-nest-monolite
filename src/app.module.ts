fe8ec89d671424d39ebd254f85cef4d96c11dd7c
shelbymode <andrew.ananenko@modsen-software.com>
2023-03-19 11:50:21 +0300

    feat(cinema): add `cinema module to root module`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { CinemaModule } from './cinema/cinema.module'
 
 @Module({
-  imports: [],
+  imports: [CinemaModule],
   controllers: [AppController],
   providers: [AppService],
 })
