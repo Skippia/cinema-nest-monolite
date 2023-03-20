2569d4b45bf21c038c07d2314d9af1a9df82629a
shelbymode <andrew.ananenko@modsen-software.com>
2023-03-20 15:47:47 +0300

    feat(root-module): add `seats-in-cinema module to root module`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { SeatsOnCinemaModule } from './seats-in-cinema/seats-in-cinema.module'
 
 @Module({
-  imports: [],
+  imports: [SeatsOnCinemaModule],
   controllers: [AppController],
   providers: [AppService],
 })
