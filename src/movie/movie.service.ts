c057a361247f8a2c9bdd46c1e36b4d38aa190c32
shelbymode <andrew.ananenko@modsen-software.com>
2023-03-21 15:39:12 +0300

    refactor(service): remove redundant `select` for prisma requests

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -25,47 +25,38 @@ export class MovieService {
     /**
      * In createMany is impossible to return created records
      * */
-    const newMovieIds = await this.prisma.$transaction(
+    const newMoviesInCinema = await this.prisma.$transaction(
       transformedMoviesToCinemaData.map((movieToCinema) =>
         this.prisma.movieOnCinema.create({
           data: movieToCinema,
-          select: {
-            movieId: true,
-          },
         }),
       ),
     )
 
-    return newMovieIds.map((m) => this.findOneMovie(m.movieId))
+    return newMoviesInCinema.map((m) => this.findOneMovie(m.movieId))
   }
 
   async findMoviesInCinema(cinemaId: number) {
-    const movieIds = await this.prisma.movieOnCinema.findMany({
+    const moviesInCinema = await this.prisma.movieOnCinema.findMany({
       where: {
         cinemaId,
       },
-      select: {
-        movieId: true,
-      },
     })
 
-    return movieIds.map((m) => this.findOneMovie(m.movieId))
+    return moviesInCinema.map((m) => this.findOneMovie(m.movieId))
   }
 
   async deleteMovieFromCinema(cinemaId: number, movieId: string) {
-    const removedMovieId = await this.prisma.movieOnCinema.delete({
+    const deletedMovieFromCinema = await this.prisma.movieOnCinema.delete({
       where: {
         cinemaId_movieId: {
           cinemaId,
           movieId,
         },
       },
-      select: {
-        movieId: true,
-      },
     })
 
-    return this.findOneMovie(removedMovieId.movieId)
+    return this.findOneMovie(deletedMovieFromCinema.movieId)
   }
 
   async resetMoviesInCinema(cinemaId: number) {
