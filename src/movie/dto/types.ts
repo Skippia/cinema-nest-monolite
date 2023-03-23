8f76191b0c89b352a21e64e85d58bc8ff3454419
shelbymode <andrew.ananenko@modsen-software.com>
2023-03-23 22:32:29 +0300

    feat(type): add `Movie type` && `update movies.json`

---
+++ src/movie/dto/types.ts
@@ -0,0 +1,15 @@
+export interface Movie {
+  title: string
+  rating: number
+  releaseYear: number
+  image: string
+  description: string
+  trailer: string
+  genres: string[]
+  authors: string[]
+  writers: string[]
+  id: string
+  actors: string[]
+  duration: number
+  countries: string[]
+}
