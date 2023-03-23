22c1bff9d8079415ffae554e30acf843b56f05a3
shelbymode <andrew.ananenko@modsen-software.com>
2023-03-23 23:49:51 +0300

    refactor(seat): refactor `controller, service`

--- src/seat/seat.service.ts
+++ src/seat/seat.service.ts
@@ -1,32 +1,33 @@
 import { Injectable } from '@nestjs/common'
+import { Seat } from '@prisma/client'
 import { PrismaService } from '../prisma/prisma.service'
 import { CreateSeatDto } from './dto/create-seat.dto'
 import { UpdateSeatDto } from './dto/update-seat.dto'
 
 @Injectable()
 export class SeatService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
-  async createSeat(seatData: CreateSeatDto) {
+  async createSeat(seatData: CreateSeatDto): Promise<Seat> {
     return await this.prisma.seat.create({ data: seatData })
   }
 
-  async findAllSeats() {
+  async findAllSeats(): Promise<Seat[]> {
     return await this.prisma.seat.findMany()
   }
 
-  async findOneSeat(id: number) {
+  async findOneSeat(id: number): Promise<Seat | null> {
     return await this.prisma.seat.findUnique({ where: { id } })
   }
 
-  async updateSeat(id: number, updateSeatData: UpdateSeatDto) {
+  async updateSeat(id: number, updateSeatData: UpdateSeatDto): Promise<Seat> {
     return await this.prisma.seat.update({
       where: { id },
       data: updateSeatData,
     })
   }
 
-  async removeSeat(id: number) {
+  async removeSeat(id: number): Promise<Seat> {
     return await this.prisma.seat.delete({ where: { id } })
   }
 }
