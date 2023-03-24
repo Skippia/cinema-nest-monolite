d35cdb24009a7a597f27d3a3b1161d4979a34691
shelbymode <andrew.ananenko@modsen-software.com>
2023-03-24 14:26:28 +0300

    refactor(dto): add `extends to some Dto classes`

--- src/cinema/dto/find-cinema.dto.ts
+++ src/cinema/dto/find-cinema.dto.ts
@@ -1,27 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { Cinema } from '@prisma/client'
-import { IsInt, IsString, MaxLength, MinLength } from 'class-validator'
+import { IsInt } from 'class-validator'
+import { CreateCinemaDto } from './create-cinema.dto'
 
-export class FindCinemaDto implements Cinema {
+export class FindCinemaDto extends CreateCinemaDto implements Cinema {
   @IsInt()
   @ApiProperty({ example: 1 })
   id: number
-
-  @IsString()
-  @MinLength(2)
-  @MaxLength(64)
-  @ApiProperty({ example: 'Dom Kino' })
-  name: string
-
-  @IsString()
-  @MinLength(2)
-  @MaxLength(64)
-  @ApiProperty({ example: 'Plotnikova 42' })
-  address: string
-
-  @IsString()
-  @MinLength(2)
-  @MaxLength(64)
-  @ApiProperty({ example: 'Minsk' })
-  city: string
 }
