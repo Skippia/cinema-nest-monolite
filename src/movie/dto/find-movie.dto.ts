50b409efc6e9f84a64b6a372534fd8b92a20060d
shelbymode <andrew.ananenko@modsen-software.com>
2023-03-24 13:08:59 +0300

    feat(movie): add `swagger examples (DTO-s)` & `method description`

--- src/movie/dto/find-movie.dto.ts
+++ src/movie/dto/find-movie.dto.ts
@@ -4,63 +4,99 @@ import { IsArray, IsInt, IsNotEmpty, IsNumber, IsString } from 'class-validator'
 
 export class FindMovieDto {
   @IsInt()
-  @ApiProperty()
-  id: number
+  @IsNotEmpty()
+  @ApiProperty({ example: 'tt0111161' })
+  id: string
 
   @IsString()
   @IsNotEmpty()
-  @ApiProperty()
+  @ApiProperty({ example: 'The Shawshank Redemption' })
   title: string
 
   @IsString()
   @IsNotEmpty()
-  @ApiProperty()
+  @ApiProperty({
+    example:
+      'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+  })
   description: string
 
   @IsInt()
   @IsNotEmpty()
-  @ApiProperty()
+  @ApiProperty({ example: 1994 })
   releaseYear: number
 
   @IsNumber()
-  @ApiProperty()
+  @ApiProperty({ example: 9.3 })
   rating: number
 
   @IsInt()
-  @ApiProperty()
+  @ApiProperty({ example: 146 })
   duration: number
 
   @IsString()
   @IsNotEmpty()
-  @ApiProperty()
+  @ApiProperty({
+    example:
+      'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_QL75_UX380_CR0,1,380,562_.jpg',
+  })
   image: string
 
   @IsString()
   @IsNotEmpty()
-  @ApiProperty()
+  @ApiProperty({ example: 'https://www.youtube.com/embed/NmzuHjWmXOc' })
   trailer: string
 
   @IsArray()
   @IsNotEmpty()
   @Type(() => String)
-  @ApiProperty({ isArray: true, type: String })
+  @ApiProperty({ isArray: true, type: String, example: ['Spain'] })
   countries: string[]
 
   @IsArray()
   @IsNotEmpty()
   @Type(() => String)
-  @ApiProperty({ isArray: true, type: String })
+  @ApiProperty({
+    isArray: true,
+    type: String,
+    example: [
+      'Tim Robbins',
+      'Morgan Freeman',
+      'Bob Gunton',
+      'William Sadler',
+      'Clancy Brown',
+      'Gil Bellows',
+      'Mark Rolston',
+      'James Whitmore',
+      'Jeffrey DeMunn',
+      'Larry Brandenburg',
+      'Neil Giuntoli',
+      'Brian Libby',
+      'David Proval',
+      'Joseph Ragno',
+      'Jude Ciccolella',
+      'Paul McCrane',
+      'Renee Blaine',
+      'Scott Mann',
+    ],
+  })
   actors: string[]
 
   @IsArray()
   @IsNotEmpty()
   @Type(() => String)
-  @ApiProperty({ isArray: true, type: String })
+  @ApiProperty({ isArray: true, type: String, example: ['Drama'] })
   genres: string[]
 
   @IsArray()
   @IsNotEmpty()
   @Type(() => String)
-  @ApiProperty({ isArray: true, type: String })
+  @ApiProperty({ isArray: true, type: String, example: ['Stephen King'] })
+  writers: string[]
+
+  @IsArray()
+  @IsNotEmpty()
+  @Type(() => String)
+  @ApiProperty({ isArray: true, type: String, example: ['Frank Darabont'] })
   authors: string[]
 }
--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, NotFoundException, Param, ParseIntPipe, UseFilters } from '@nestjs/common'
 import { MovieService } from './movie.service'
 import { FindMovieDto } from './dto/find-movie.dto'
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger'
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger'
 import { PrismaClientExceptionFilter } from '../prisma/prisma-client-exception'
 import { Movie } from './dto/types'
 
@@ -12,6 +12,7 @@ export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
   @Get()
+  @ApiOperation({ description: 'Get all movies' })
   @ApiOkResponse({ type: FindMovieDto, isArray: true })
   findAllMovies(): Movie[] {
     const movies = this.movieService.findAllMovies()
@@ -20,6 +21,7 @@ export class MovieController {
   }
 
   @Get(':movieId')
+  @ApiOperation({ description: 'Get one movie by movieId (from MovieRecord)' })
   @ApiOkResponse({ type: FindMovieDto })
   async findOneMovie(@Param('movieId', ParseIntPipe) movieId: number): Promise<Movie> {
     const movie = await this.movieService.findOneMovie(movieId)
