16e30a6d8011d92787304c0e11a72de6c248fe68
shelbymode <andrew.ananenko@modsen-software.com>
2023-03-28 09:28:53 +0300

    feat(dto): update `movie-session DTO-s`

--- src/movie-session/dto/create-movie-session.dto.ts
+++ src/movie-session/dto/create-movie-session.dto.ts
@@ -3,16 +3,11 @@ import { MovieSession } from '@prisma/client'
 import { Type } from 'class-transformer'
 import { IsInt, IsDate } from 'class-validator'
 
-export class CreateMovieSessionDto implements Omit<MovieSession, 'id'> {
+export class CreateMovieSessionDto implements Omit<MovieSession, 'id' | 'endDate'> {
   @Type(() => Date)
   @IsDate()
   @ApiProperty({ example: '2023-03-24T10:25:01.504Z' })
   startDate: Date
-
-  @Type(() => Date)
-  @IsDate()
-  @ApiProperty({ example: '2023-03-24T10:25:01.504Z' })
-  endDate: Date
 
   @IsInt()
   @ApiProperty({ example: 1 })
--- src/movie-session/dto/find-movie-session.dto.ts
+++ src/movie-session/dto/find-movie-session.dto.ts
@@ -1,10 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { MovieSession } from '@prisma/client'
-import { IsInt } from 'class-validator'
+import { Type } from 'class-transformer'
+import { IsDate, IsInt } from 'class-validator'
 import { CreateMovieSessionDto } from './create-movie-session.dto'
 
 export class FindMovieSessionDto extends CreateMovieSessionDto implements MovieSession {
   @IsInt()
   @ApiProperty({ example: 1 })
   id: number
+
+  @IsDate()
+  @Type(() => Date)
+  @ApiProperty()
+  endDate: Date
 }
