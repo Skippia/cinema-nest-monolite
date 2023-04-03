79dcddffbbc3f93b5af59076efcc6051b9e5c47d
shelbymode <andrew.ananenko@modsen-software.com>
2023-04-03 16:26:01 +0300

    refactor(seats-in-cinema): refactor seats-in-cinema service
    - refactor createCinemaSeatingSchema method

--- src/seats-in-cinema/seats-in-cinema.service.ts
+++ src/seats-in-cinema/seats-in-cinema.service.ts
@@ -1,45 +1,86 @@
 import { Injectable } from '@nestjs/common'
-import { Seat, SeatOnCinema } from '@prisma/client'
+import { Seat, SeatOnCinema, TypeSeat, TypeSeatEnum } from '@prisma/client'
 import { PrismaService } from '../prisma/prisma.service'
-import { generateSeatSchema, recoverySeatSchema } from '../utils/seatsInCinema/helpers'
-import { ISeatPos, ISeatSchemaOutput } from '../utils/seatsInCinema/types'
 import { CreateCinemaSeatingSchemaDto } from './dto/create-cinema-seating-plan.dto'
+import { SeatService } from '../seat/seat.service'
+import { ChainSeatsOverrider } from './utils/OverrideSeats/ChainSeatsOverrider'
+import { generateBaseSeatSchema } from './utils/helpers/generateBaseSeatSchema'
+import { overrideWithLoveSeats } from './utils/OverrideSeats/overrideWithLoveSeats'
+import { overrideWithVipSeats } from './utils/OverrideSeats/overrideWithVipSeats'
+import { ISeatsSchema, ISeatPosWithType } from '../utils/types'
 
 @Injectable()
 export class SeatsInCinemaService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService, private readonly seatService: SeatService) {}
 
   async createCinemaSeatingSchema(
     cinemaId: number,
     seatsSchemaData: CreateCinemaSeatingSchemaDto,
-  ): Promise<ISeatSchemaOutput> {
+  ): Promise<ISeatsSchema> {
     /**
-     * Generate seats schema with seats (only seats, without empties)
+     * Generate seats schema (only seats, without empties)
      */
-    const seatsSchema = generateSeatSchema(seatsSchemaData)
+    const { vipSeats = [], loveSeats = [] } = seatsSchemaData
+    const basedSeatsSchema = generateBaseSeatSchema(seatsSchemaData)
+
+    const chainSeatsOverrider = new ChainSeatsOverrider({
+      source: basedSeatsSchema,
+      mappers: [overrideWithVipSeats(vipSeats), overrideWithLoveSeats(loveSeats)],
+    })
+
+    const processedSchema = chainSeatsOverrider.run()
 
     /**
-     * Replace seats for real seats from db
+     * Replace seats for real seats or create
      */
-    const realSeatsSchema = await this.replacePositionsToRealSeats(seatsSchema)
+    const realSeatsSchema = await this.seatService.replaceSeatsPositionsToRealOrCreateSeats(processedSchema)
+
+    const { id: defaultSeatTypeId } = await this.seatService.findSeatTypeIdBySeatType(TypeSeatEnum.SEAT)
+    const { id: vipSeatTypeId } = await this.seatService.findSeatTypeIdBySeatType(TypeSeatEnum.VIP)
+    const { id: loveSeatTypeId } = await this.seatService.findSeatTypeIdBySeatType(TypeSeatEnum.LOVE)
 
     /**
      * Add generated seats from realSeatsSchema to cinema (db)
      */
-    const generatedSeats = await this.addSeatsSchemaToCinema(cinemaId, realSeatsSchema)
+    const addedSeatsToCinemaArray = await this.addSeatsToCinemaByType(cinemaId, [
+      {
+        schema: realSeatsSchema,
+        typeSeat: { id: defaultSeatTypeId, type: TypeSeatEnum.SEAT },
+      },
+      {
+        schema: realSeatsSchema,
+        typeSeat: { id: vipSeatTypeId, type: TypeSeatEnum.VIP },
+      },
+      {
+        schema: realSeatsSchema,
+        typeSeat: { id: loveSeatTypeId, type: TypeSeatEnum.LOVE },
+      },
+    ])
 
-    /**
-     * Recover information about empties and return it
-     */
-    return recoverySeatSchema(generatedSeats)
+    return addedSeatsToCinemaArray
   }
 
-  /**
-   * Helper for createCinemaSeatingSchema
-   */
-  async addSeatsSchemaToCinema(cinemaId: number, seats: Seat[]): Promise<ISeatPos[]> {
+  async addSeatsToCinemaByType(
+    cinemaId: number,
+    data: { schema: (ISeatPosWithType & { id: number })[]; typeSeat: TypeSeat }[],
+  ): Promise<ISeatPosWithType[]> {
+    const addedSeatsToCinemaDoubleArray = await Promise.all(
+      data.map((el) =>
+        this.addSeatsSchemaToCinema(
+          cinemaId,
+          el.schema.filter((e) => e.type === el.typeSeat.type),
+          el.typeSeat.id,
+        ),
+      ),
+    )
+    const addedSeatsToCinemaArray = addedSeatsToCinemaDoubleArray.flatMap((el) => el)
+
+    return addedSeatsToCinemaArray
+  }
+
+  async addSeatsSchemaToCinema(cinemaId: number, seats: Seat[], typeSeatId: number): Promise<ISeatPosWithType[]> {
     const seatCinemaElements = seats.reduce(
-      (acc, seat) => [...acc, { cinemaId, seatId: seat.id }],
+      (acc, seat) => [...acc, { cinemaId, seatId: seat.id, typeSeatId }],
       [] as SeatOnCinema[],
     )
 
@@ -52,6 +93,7 @@ export class SeatsInCinemaService {
           data: seatCinemaElement,
           include: {
             seat: true,
+            type: true,
           },
         }),
       ),
@@ -59,59 +101,26 @@ export class SeatsInCinemaService {
     return data.map((seatFull) => ({
       row: seatFull.seat.row,
       col: seatFull.seat.col,
+      type: seatFull.type.type,
     }))
   }
 
-  /**
-   * Helper for createCinemaSeatingSchema
-   */
-  async replacePositionsToRealSeats(seats: ISeatPos[]): Promise<Seat[]> {
-    const realSeats: Seat[] = []
-
-    for (const { col, row } of seats) {
-      /**
-       * Try to get all needed seats according to the seat schema
-       */
-      let realSeat = await this.prisma.seat.findUnique({
-        where: {
-          row_col: {
-            col,
-            row,
-          },
-        },
-      })
-
-      /**
-       * If such seat yet doesn't exist - create it
-       */
-      if (!realSeat) {
-        realSeat = await this.prisma.seat.create({
-          data: { col, row },
-        })
-      }
-
-      realSeats.push(realSeat)
-    }
-
-    return realSeats
-  }
-
-  async findCinemaSeatingSchema(cinemaId: number): Promise<ISeatSchemaOutput> {
+  async findCinemaSeatingSchema(cinemaId: number): Promise<ISeatsSchema> {
     const seats = await this.prisma.seatOnCinema.findMany({
       where: {
         cinemaId,
       },
       include: {
         seat: true,
+        type: true,
       },
     })
 
-    return recoverySeatSchema(
-      seats.map((seatFull) => ({
-        row: seatFull.seat.row,
-        col: seatFull.seat.col,
-      })),
-    )
+    return seats.map((seatFull) => ({
+      row: seatFull.seat.row,
+      col: seatFull.seat.col,
+      type: seatFull.type.type,
+    }))
   }
 
   async resetCinemaSeatingSchema(cinemaId: number) {
