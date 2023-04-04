6b9289be01887d5fd04834a87ac5f98e8d78811f
shelbymode <andrew.ananenko@modsen-software.com>
2023-04-04 16:42:59 +0300

    feat(dto): add custom validator `isRecord<Enum, Type>`

---
+++ src/utils/customValidators/isRecord.ts
@@ -0,0 +1,27 @@
+import { ValidationOptions, registerDecorator, ValidationArguments, isObject } from 'class-validator'
+
+export function IsRecord(
+  object_needed: Record<string, unknown>,
+  value_validator: (value: unknown) => boolean,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: unknown, propertyName: string) {
+    registerDecorator({
+      name: 'isRecord',
+      target: (object as any).constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: Record<string, unknown>, args: ValidationArguments) {
+          if (!isObject(value)) return false
+
+          const isRecordCorresponds = Object.keys(object_needed).every(
+            (x) => Object.keys(value).includes(x) && value_validator(value[x]),
+          )
+
+          return isRecordCorresponds
+        },
+      },
+    })
+  }
+}
