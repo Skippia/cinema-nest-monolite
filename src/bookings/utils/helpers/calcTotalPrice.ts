04e31e46ee7e723f45bec3a41b0c100e529364d1
shelbymode <andrew.ananenko@modsen-software.com>
2023-04-05 11:54:01 +0300

    feat(helpers): add `booking helpers`

---
+++ src/bookings/utils/helpers/calcTotalPrice.ts
@@ -0,0 +1,13 @@
+import { MovieSession, TypeSeat } from '@prisma/client'
+import { ISeatsSchema } from 'src/utils/types'
+
+export const calcTotalPrice = (
+  seatsSchema: ISeatsSchema,
+  multiFactorsArr: { priceFactor: number; typeSeat: TypeSeat }[],
+  movieSession: MovieSession,
+) =>
+  seatsSchema.reduce((totalPrice, curSeat) => {
+    const priceForCurrentSeat =
+      movieSession.price * (multiFactorsArr.find((factor) => factor.typeSeat.type === curSeat.type)?.priceFactor || 1)
+    return totalPrice + priceForCurrentSeat
+  }, 0)
