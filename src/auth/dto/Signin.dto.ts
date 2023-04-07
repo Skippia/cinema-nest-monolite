b648ccd2d75f2cdacf23133d0a9aac9db0ac39b7
shelbymode <andrew.ananenko@modsen-software.com>
2023-04-07 16:35:25 +0300

    feat(dto): add `auth dto-s`

---
+++ src/auth/dto/Signin.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
+import { ISigninDto } from '../utils/types/dto.type'
+
+export class SigninDto implements ISigninDto {
+  @IsEmail()
+  @ApiProperty({ example: 'john@example.com' })
+  email: string
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({ example: 'password123' })
+  password: string
+}
