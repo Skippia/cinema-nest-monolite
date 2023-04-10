3c2980e58cd2c09ad04fdbbc22e83ed218d5dde1
shelbymode <andrew.ananenko@modsen-software.com>
2023-04-10 17:10:05 +0300

    feat(swagger): fix `swagger`
    - fix swagger UI for deployment
    - add global prefix for api

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,40 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieParser = require('cookie-parser')
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  // app.enableCors()
-  app.use(cookieParser())
-
-  app.useGlobalPipes(new ValidationPipe())
+  const app: NestExpressApplication = await NestFactory.create(AppModule)
 
   const config = new DocumentBuilder()
-    .setTitle('Modsen-Cinema')
-    .setDescription('The Cinema API description')
-    .setVersion('0.1')
+    .setTitle('Modsen Cinema')
+    .setDescription('The Cinema API')
+    .setVersion('1.0.0')
+    .setBasePath('api/v1')
     .build()
+
   const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, document)
+
+  SwaggerModule.setup('docs', app, document, {
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  })
+
+  // app.enableCors(`)
+  app.use(cookieParser())
+  app.useGlobalPipes(new ValidationPipe())
 
   await app.listen(3000)
 }
+
 bootstrap()
