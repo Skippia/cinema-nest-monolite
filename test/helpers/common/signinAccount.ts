17e91343279b5391ff3ad1c8e635214cce4e8d79
shelbymode <andrew.ananenko@modsen-software.com>
2023-04-28 01:12:20 +0300

    fix(e2e): change `user role to admin for test account (for e2e tests)`

--- test/helpers/common/signinAccount.ts
+++ test/helpers/common/signinAccount.ts
@@ -3,7 +3,7 @@ import request from 'supertest'
 
 export async function signinAccount(app: INestApplication): Promise<string[]> {
   const credentials = {
-    email: 'pocketbook.love24@gmail.com',
+    email: 'admin@gmail.com',
     password: 'midapa',
   }
 
--- test/helpers/create/createUsers.ts
+++ test/helpers/create/createUsers.ts
@@ -1,15 +1,15 @@
 import * as bcrypt from 'bcrypt'
-import { AuthProviderEnum, User } from '@prisma/client'
+import { AuthProviderEnum, RoleEnum, User } from '@prisma/client'
 import { PrismaService } from '../../../src/modules/prisma/prisma.service'
 
 export async function createUsers(prisma: PrismaService): Promise<User> {
   return await prisma.user.create({
     data: {
-      email: 'pocketbook.love24@gmail.com',
+      email: 'admin@gmail.com',
       firstName: 'John',
       lastName: 'Doe',
       hashedPassword: bcrypt.hashSync('midapa', 10),
-      role: 'USER',
+      role: RoleEnum.ADMIN,
       gender: 'MALE',
       language: 'EN',
       provider: AuthProviderEnum.LOCAL,
