72073b4e039c31045230981a15a396eb9a4dd3bf
shelbymode <andrew.ananenko@modsen-software.com>
2023-05-02 15:30:35 +0300

    feat: add movies.json

--- src/modules/movie-session/movie-session.module.ts
+++ src/modules/movie-session/movie-session.module.ts
@@ -4,10 +4,12 @@ import { MovieSessionController } from './movie-session.controller'
 import { MoviesInCinemaService } from '../movies-in-cinema/movies-in-cinema.service'
 import { MovieService } from '../movie/movie.service'
 import { CinemaService } from '../cinema/cinema.service'
+import { S3Module } from '../s3/s3.module'
 
 @Module({
   controllers: [MovieSessionController],
   providers: [MovieSessionService, MoviesInCinemaService, MovieService, CinemaService],
   exports: [MovieSessionService],
+  imports: [S3Module],
 })
 export class MovieSessionModule {}
